fba305580de0b626d68eaa50ab30c955a7a54910
Hugo COMBET <hugo.combet@saintmichelannecy.fr>
2020-09-11 11:53:31 +0200

    Ajoutez des fichiers projet.

---
+++ ClubFromage/DBConnect.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace ClubFromage
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "ClubFromage";
+            uid = "root";
+            password = "5MichelAnnecy";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        public void ExectQuery(string query)
+        {
+            connection.Open();
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            //pour éxécuter:
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Insert(string query)
+        {
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+        public void Update(string query)
+        {
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //Assign the query using CommandText
+                cmd.CommandText = query;
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+        public void Delete(string query)
+        {
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+    }
+}
---
+++ ClubFromage/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using Renci.SshNet.Security.Cryptography;
+using System;
+
+namespace ClubFromage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnect ClubFromage = new DBConnect();
+            //string requete = "INSERT into Pays (Id,nom) VALUES(1,'France')";
+            //string requete = "INSERT into Pays (Id,nom) VALUES(1,'Belgique')";
+            //string requete = "INSERT into Fromage (Id,PaysOrigine_id,nom,creation,image) VALUES(2,2,'saintAlbray','2020/09/09','aa')";
+            //string requete1 = "INSERT into Fromage (Id,PaysOrigine_id,nom,creation,image) VALUES(3,1,'Tomme','2020/09/01','bb')";
+
+            //ClubFromage.Insert(requete);
+            //string requete1 = "delete from fromage where Id=1";
+            string requete1 = "Update Fromage set Id=78 where Id=5";
+            ClubFromage.ExectQuery(requete1);
+        }
+    }
+}
